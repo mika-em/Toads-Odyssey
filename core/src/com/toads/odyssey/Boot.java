9d089b913c96a17ad1f1391932624d7382194cf7
mikaem <mkmanaligod@gmail.com>
2023-11-22 15:41:30 -0800

    Polish Boot.java instance variables

--- core/src/com/toads/odyssey/Boot.java
+++ core/src/com/toads/odyssey/Boot.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 public class Boot extends Game {
 
     public static Boot INSTANCE;
+    public int widthScreen, heightScreen;
+    public OrthographicCamera camera;
 
     public Boot() {
         INSTANCE = this;
@@ -14,10 +16,10 @@ public class Boot extends Game {
 
     @Override
     public void create() {
-        int widthScreen = Gdx.graphics.getWidth();
-        int heightscreen = Gdx.graphics.getHeight();
-        OrthographicCamera camera = new OrthographicCamera();
-        camera.setToOrtho(false, widthScreen, heightscreen);
+        this.widthScreen = Gdx.graphics.getWidth();
+        this.heightScreen = Gdx.graphics.getHeight();
+        this.camera = new OrthographicCamera();
+        this.camera.setToOrtho(false, this.widthScreen, this.heightScreen);
         setScreen(new GameScreen(camera));
     }
 }

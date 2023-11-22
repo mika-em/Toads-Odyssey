040962da2dcadbdf120242bf8fa09cb8657bb033
Joanne Ho <cyh66368@gmail.com>
2023-11-22 11:43:23 -0800

    animate the idle frog on screen

--- core/src/com/toads/odyssey/ToadsOdyssey.java
+++ core/src/com/toads/odyssey/ToadsOdyssey.java
@@ -1,17 +1,22 @@
 package com.toads.odyssey;
+
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.Gdx;
+import com.toads.odyssey.util.AssetsLoader;
 
 public class ToadsOdyssey extends ApplicationAdapter {
 	SpriteBatch batch;
+    public AssetsLoader assetsLoader;
+    private float elapsedTime = 0;
 	Texture img;
-	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+        assetsLoader = new AssetsLoader();
 		img = new Texture("forest.jpg");
 	}
 
@@ -23,12 +28,15 @@ public class ToadsOdyssey extends ApplicationAdapter {
 		float screenHeight = Gdx.graphics.getHeight();
 		batch.draw(img, 0, 0, screenWidth, screenHeight);
 
+        elapsedTime += Gdx.graphics.getDeltaTime();
+        TextureRegion currentFrame = assetsLoader.playerAssets.idleAnimation.getKeyFrame(elapsedTime, true);
+        batch.draw(currentFrame, 100, 100);
 		batch.end();
 	}
-	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+        assetsLoader.dispose();
 	}
 }
--- core/src/com/toads/odyssey/util/AssetsLoader.java
+++ core/src/com/toads/odyssey/util/AssetsLoader.java
@@ -1,12 +1,35 @@
 package com.toads.odyssey.util;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
 
 public class AssetsLoader implements Disposable {
+    private AssetManager manager;
+    public final PlayerAssets playerAssets;
     public AssetsLoader() {
+        this.manager = new AssetManager();
+        manager.load("spriteSheet.atlas", TextureAtlas.class);
+        manager.finishLoading();
+        TextureAtlas atlas = manager.get("spriteSheet.atlas", TextureAtlas.class);
+        playerAssets = new PlayerAssets(atlas);
+    }
+
+    public class PlayerAssets {
+        public final Animation<TextureAtlas.AtlasRegion> idleAnimation;
+        public PlayerAssets(TextureAtlas atlas) {
+            Array<TextureAtlas.AtlasRegion> idleFrames = new Array<>();
+            for (int i = 1; i <= 4; i++) {
+                idleFrames.add(atlas.findRegion("frog_idle", i));
+            }
+            idleAnimation = new Animation<>(0.1f, idleFrames, Animation.PlayMode.LOOP);
+        }
     }
 
     @Override
     public void dispose() {
+        manager.dispose();
     }
 }

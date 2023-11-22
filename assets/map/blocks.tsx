9563cfaeca046d3b8749a9223a15a7dbcf76d2f1
Joanne Ho <cyh66368@gmail.com>
2023-11-21 22:08:21 -0800

    add sprite sheet atlas and tile map (TBR)

---
+++ assets/map/blocks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="blocks" tilewidth="32" tileheight="32" tilecount="21" columns="7">
+ <image source="../Assets/FroggyForestAssetPack/blocks.png" width="224" height="96"/>
+</tileset>

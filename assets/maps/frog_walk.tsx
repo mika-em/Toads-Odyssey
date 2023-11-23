e810a7b5969e3e5af65d734235e3c986e440ad89
mikaem <mkmanaligod@gmail.com>
2023-11-23 14:55:25 -0800

    Add GameEntity and Player class

---
+++ assets/maps/frog_walk.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="frog_walk" tilewidth="16" tileheight="16" tilecount="24" columns="12">
+ <image source="../../../../Downloads/FroggyForestAssetPack 3/frog_walk.png" width="192" height="32"/>
+</tileset>
---
+++ core/src/objects/player/GameEntity.java
@@ -0,0 +1,29 @@
+package objects.player;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Body;
+
+public abstract class GameEntity {
+
+    protected float x, y, velocityX, velocityY, speed, width, height;
+    protected Body body;
+
+    public GameEntity(float width, float height, Body body) {
+        this.x = body.getPosition().x;
+        this.y = body.getPosition().y;
+        this.width = width;
+        this.height = height;
+        this.body = body;
+        this.speed = 0;
+        this.velocityX = 0;
+        this.velocityY = 0;
+    }
+
+    public abstract void update();
+
+    public abstract void render(SpriteBatch batch);
+
+    public Body getBody() {
+        return body;
+    }
+}

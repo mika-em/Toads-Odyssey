5f3ab01010f93768538e2b1f8033e7f8d31c2b69
mikaem <mkmanaligod@gmail.com>
2023-11-25 05:54:31 -0800

    Add coin assets

---
+++ assets/maps/coin_yellow.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="coin_yellow" tilewidth="16" tileheight="16" tilecount="16" columns="8">
+ <image source="../../../tiles/coin_yellow.png" width="128" height="32"/>
+</tileset>
---
+++ assets/maps/coins.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="coins" tilewidth="8" tileheight="8" tilecount="64" columns="16">
+ <image source="../tiles/coin_yellow.png" width="128" height="32"/>
+</tileset>

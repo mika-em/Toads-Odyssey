321af9605c7b07ab911fe0d05394c8ee13a59cc7
Joanne Ho <cyh66368@gmail.com>
2023-11-28 20:11:10 -0800

    set up start screen

---
+++ core/src/com/toads/odyssey/view/StartScreen.java
@@ -0,0 +1,49 @@
+package com.toads.odyssey.view;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.toads.odyssey.ToadsOdyssey;
+
+public class StartScreen implements Screen {
+    private ToadsOdyssey game;
+    private Viewport viewport;
+
+    public StartScreen(ToadsOdyssey game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}

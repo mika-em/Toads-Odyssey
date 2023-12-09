94329d7173ef8ca8f87a0839bda6e3911071ab18
mikaem <mkmanaligod@gmail.com>
2023-12-09 14:59:32 -0800

    Refactor and add java docs to DesktopLauncher

--- desktop/src/com/toads/odyssey/DesktopLauncher.java
+++ desktop/src/com/toads/odyssey/DesktopLauncher.java
@@ -3,11 +3,37 @@ package com.toads.odyssey;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+/**
+ * Runs the game on desktop.
+ *
+ * @author Joanne, Mika
+ * @version 2023
+ */
+public final class DesktopLauncher {
+    /**
+     * The width of the screen.
+     */
+    public static final int SCREEN_WIDTH = 1280;
+    /**
+     * The height of the screen.
+     */
+    public static final int SCREEN_HEIGHT = 720;
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private DesktopLauncher() {
+    }
+
+    /**
+     * Drives the game on desktop.
+     *
+     * @param arg the arguments
+     */
+    public static void main(final String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setWindowedMode(1280, 720);
+        config.setWindowedMode(SCREEN_WIDTH, SCREEN_HEIGHT);
         config.setTitle("Toad's Odyssey");
         new Lwjgl3Application(new ToadsOdyssey(), config);
-	}
+    }
 }
